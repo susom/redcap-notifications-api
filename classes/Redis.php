c31be7b436ae8866d1716d34a2869576d28c5470
Jordan Schultz <jmschultz@ucdavis.edu>
2024-02-06 15:14:07 -0800

    Implemented hset and hget, added sets for hash keys

--- classes/Redis.php
+++ classes/Redis.php
@@ -18,23 +18,59 @@ class Redis implements CacheInterface
         ]);
     }
 
-    public function setKey($key, $value): \Predis\Response\Status
+    public function setKey($key, $value): void
     {
-        return $this->client->set($key, $value);
+        //Grab notification ID from pre-generated key
+        $explode = explode("_", $key);
+        $notification_id = $explode[3];
+
+        //Notification ID will be hashed in redis, remove
+        unset($explode[3]);
+        $storage_key = implode("_", $explode);
+
+        //Add key as PID_[PROD/DEV]_[ROLE] as key, setting hash as notification ID
+        $this->client->hset($storage_key, $notification_id, $value);
+
+        //Keep track of corresponding keyset for performance later in getKEY
+        $this->client->sadd("keyset_".$storage_key, [$notification_id]);
+
     }
 
     public function setKeys(array $arr): \Predis\Response\Status
     {
+//        TODO
         return $this->client->mset($arr);
     }
 
     public function getKey($key): ?string
     {
-        return $this->client->get($key);
+        $explode = explode("_", $key);
+        $notification_id = $explode[3];
+
+        unset($explode[3]);
+        $storage_key = implode("_", $explode);
+
+        return $this->client->hget($storage_key, $notification_id);
     }
+
+    /**
+     * @param $key
+     * @return void
+     */
+    public function getAllHashed($key): array
+    {
+        // Expecting key in the format PID_[PROD/DEV]_ROLE
+        $set = $this->client->smembers("keyset_".$key);
+        $arr = [];
+        foreach($set as $hash)
+            $arr[] = $this->client->hget($key, $hash);
+        return $arr;
+    }
+
 
     public function getKeys(array $arr): array
     {
+//        TODO
         return $this->client->mget($arr);
     }
 
@@ -57,6 +93,26 @@ class Redis implements CacheInterface
     {
         // TODO: Implement expireKey() method.
     }
+
+    public function getRedisClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     *
+     * @param $pattern
+     * @return void
+     */
+    public function search($pattern){
+        $output = "";
+        echo "Testing Scan for pattern key*";
+        foreach (new Iterator\Keyspace($this->getRedisClient(), $pattern) as $key) {
+            $output .= "$key \n";
+        }
+        var_dump($output);
+    }
+
 
     public function info(): array
     {
--- pages/TestPage.php
+++ pages/TestPage.php
@@ -2,34 +2,47 @@
 
 // namespace Stanford\RedcapNotificationsAPI;
 /** @var \Stanford\RedcapNotificationsAPI\RedcapNotificationsAPI $module*/
+use \Predis\Collection\Iterator;
 
 try {
 //    Create new client
     $factory = new \Stanford\RedcapNotificationsAPI\CacheFactory();
     $client = $factory->getCacheClient( 'redis', '6379');
 
-    echo "Set Key `key1`";
-    $a = $client->setKey('key1','value1');
+    echo "Set Key '1_prod_all_2' => '{data:value}' also setting keyset ";
+    $a = $client->setKey('1_prod_all_2','{data:value}');
     var_dump($client->listKeys("*"));
 
     echo "Set Keys `key2, key3`";
     $b = $client->setKeys(['key2' =>'value2', 'key3' => 'value3']);
     var_dump($client->listKeys("*"));
 
-    echo "Get Key `key1`";
-    $c = $client->getKey("key1");
+    echo "Get Key '1_prod_all_2'";
+    $c = $client->getKey("1_prod_all_2");
     var_dump($c);
 
     echo "Get Keys `key2, key3`";
     $d = $client->getKeys(["key2", "key3"]);
     var_dump($d);
 
-    echo "Delete Key `key1`";
-    $e = $client->deleteKey("key1");
+    $pattern = 'key*';
+    $output = "";
+    echo "Testing Scan for pattern key*";
+    foreach (new Iterator\Keyspace($client->getRedisClient(), $pattern) as $key) {
+        $output .= "$key \n";
+    }
+    var_dump($output);
+
+    echo "Testing getAllHashed";
+    var_dump($client->getAllHashed("1_prod_all"));
+
+
+    echo "Delete Key `1_prod_all_1`";
+    $e = $client->deleteKey("1_prod_all");
     var_dump($client->listKeys("*"));
 
-    echo "Delete Keys `key2, key3`";
-    $f = $client->deleteKeys(["key2", "key3"]);
+    echo "Delete Keys `key2, key3, keyset_1_prod_all`";
+    $f = $client->deleteKeys(["key2", "key3", "keyset_1_prod_all"]);
     var_dump($client->listKeys("*"));
 
 
