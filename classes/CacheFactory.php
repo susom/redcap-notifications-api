2ee1c15fd924c131e7959d883f6c9ed63483fae6
ihabz <ihab.zeedia@stanford.edu>
2024-05-31 10:29:20 -0700

    fix php 8.2 error.

--- classes/CacheFactory.php
+++ classes/CacheFactory.php
@@ -4,7 +4,7 @@ namespace Stanford\RedcapNotificationsAPI;
 
 class CacheFactory
 {
-    public static function getCacheClient($type, $host = '', $port = '', $luaPath)
+    public static function getCacheClient($type, $host = '', $port = '', $luaPath = '')
     {
         if($type == 'redis' and !empty($host) and !empty($port) and !empty($luaPath)){
             return new Redis($host, $port, $luaPath);
